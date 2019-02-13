0c922c5571c17beb7ba72df48747a8d989722149
Paraprax <parkerjohnston28@gmail.com>
2019-02-13 16:35:38 -0500

    added the htmlRoutes.js file

---
+++ routes_controller/htmlRoutes.js
@@ -0,0 +1,22 @@
+//Dependencies
+// the 'path' package for the join function to get the correct file path in the route to our html 
+var path = require("path");
+
+//Routing
+
+module.exports = function(app) {
+    //handle 'get' requests eg. when a user visits a page via a url pathway, serve them the homepage html file
+
+    app.get("/booths", function(req,res) {
+        res.sendFile(path.join(__dirname, "../public_view/booths.html"));
+    })
+
+    app.get("/reserve", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public_view/reserve.html"));
+    });
+
+    //if no matching pathway is found, default to serving them the homepage
+    app.get("*", function(req,res) {
+        res.sendFile(path.join(__dirname, "../public_view/homepage.html"));
+    })
+}
